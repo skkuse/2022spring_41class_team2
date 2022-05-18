161c895a36a889b1d33e80e03d0b164fe623df01
BaeJihyun97 <baejihyun970519@gmail.com>
2022-05-18 13:21:28 +0900

    UserInfo base style update

--- client/src/pages/UserInfo.js
+++ client/src/pages/UserInfo.js
@@ -1,6 +1,8 @@
 import React, { useState } from 'react';
 import {call} from '../service/APIService';
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
+import '../css/UserInfo.css'
+//import my_lectures from "../components/image/json1.json";
 
 function UserInfoPage() {
   const [username, setUserName] = useState('');
@@ -31,15 +33,60 @@ function UserInfoPage() {
 */
 
   return (
-    <div>
-        <h1> 이름 : {username}</h1>
-        <h1> 이메일 : {userEmail}</h1>
-        <Link to = "/codeEdit">
-          <button>코드를 쳐볼까요?</button>
-        </Link>
+	  <div class="background">
+		  <div class="warp">
+			  <div id="upper">
+				  <div class="v12_15">
+					  <span class="v5_31">CrawlLearn</span>
+					  <div class="v5_30">
+						  <div class="v5_32">도움말</div>
+						  <div class="v5_32">ABOUT US</div>
+					  </div>
+				  </div>
 
+			  </div>
+			  <div class="profile">
+				  <div class="account">
+					  <div class="title_info">
+						  <span>기본정보</span>
+						  <button class="button_greenM" type="button">변경하기</button>
+					  </div>
+					  <div class="my_name">
+						  <span>이름</span><br/>
+						  <input id="my_name_change"></input>
+					  </div>
+						  <div class="my_email">
+							  <span>이메일</span><br/>
+								  <div id="email_addr">xxx@g.skku.edu</div>
+						  </div>
+						  </div>
+						  <div class="activity">
+							  <div class="title_info">
+								  <span>활동정보</span>
+							  </div>
+							  <div id="my_registered">
+								  <select>
+									  <option value="">Registered</option>
+									  {my_lectures.map((lectureE) => <option value={lectureE.lecture_content_seq} >{lectureE.lecture_name}</option>)}
+								  </select>
+							  </div>
+							  <div id="my_qa">
+								  <select>
+									  <option value="">QA</option>
+									  {my_lectures.map((lectureE) => <option value={lectureE.lecture_content_seq} >{lectureE.lecture_name}</option>)}	
+								  </select>
+							  </div>
+							  <div id="my_liked">
+							      <select>
+										<option value="">Like</option>
+										{my_lectures.map((lectureE) => <option value={lectureE.lecture_content_seq} >{lectureE.lecture_name}</option>)}
+								  </select>
+							  </div>
+						  </div>
+					  </div>
+				  </div>
 
-    </div>
+			  </div>
   );
 }
 
