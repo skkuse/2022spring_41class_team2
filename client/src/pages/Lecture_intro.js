3dec14b9815e55d066810e2144d4ce48382f98e8
BaeJihyun97 <baejihyun970519@gmail.com>
2022-05-25 14:02:37 +0900

    lecture intro css and register data representation update

--- client/src/pages/Lecture_intro.js
+++ client/src/pages/Lecture_intro.js
@@ -7,10 +7,11 @@ import rehypeRaw from 'rehype-raw'
 
 function Lecture_intro() {
     const location = useLocation();
-    console.log(location);
     const data = location.state.data;
 
     const [lecture_content, set_lecture_content] = useState("");
+    const [completed, setcompleted] = useState("");
+    const [attending, setattending] = useState(0);
 
     const attending_lecture = () => {
         setDisable(false);
@@ -28,12 +29,73 @@ function Lecture_intro() {
 
                     }
                     else {
+
                         set_lecture_content(response['data']);
                     }
 
                 }
             )
     }
+
+
+
+    /*async function fetchData() {
+        let queryString = '/lectures/'+data.lecture_seq+'/lectureContent/' + data.lecture_content_seq + '/userSeq/' + +'/1';
+        call(queryString, "GET")
+            .then(
+                response => {
+                    setlectures(response['data'])
+                    setLectureSort("")
+                }
+            )
+        setLoading(false);
+    }*/
+
+    const getUserInfo2 = () => {
+        var token = sessionStorage.getItem("ACCESS_TOKEN");
+        if (token == null) {
+            window.location.href = '/login';
+        }
+        call("/user/info", "GET")
+            .then(
+                response => {
+                    if (response['data']['attending_lecture']) {
+                        if (response['data']['attending_lecture'].includes(data.lecture_content_title)) { setattending(1) }
+                        console.log(response['data']['attending_lecture'])
+                        console.log(data.lecture_content_title)
+                    }
+                }
+            ).catch(
+                error => {
+                    console.log(error)
+                }
+            )
+    }
+
+    useEffect(() => {
+        getUserInfo2();
+    }, []);
+
+    useEffect(() => {
+        setDisable(false);
+        if (attending != 0) {
+            call("/lectures/lectureContents/" + data.lecture_content_seq, "GET")
+                .then(
+                    response => {
+
+                        if (response['status_code'] == 400) {
+
+                        }
+                        else {
+
+                            set_lecture_content(response['data']);
+                        }
+
+                    }
+                )
+        }
+
+    }, [attending]);
 
 
     const [disable, setDisable] = useState(true);
@@ -44,59 +106,60 @@ function Lecture_intro() {
 
     return (
         <div className='Lecture_intro'>
+            <div className='lec_header'>
+                <br></br>
+                <div className='lec_he0' ><p>CrawlLearn</p></div>
+                <div className='lec_he2' >{
+                    {
+                        1: <p>Basic Python</p>,
+                        2: <p>Web Page Structure</p>,
+                        3: <p>Web Crawling</p>
+                    }[sequence]
+                }</div>
+            </div>
 
-            <body>
-                <div class='lec_header'>
-                    <br></br>
-                    <div class='lec_he0' >CrawlLearn</div>
-                    <div class='lec_he1' >강의 대분류</div>
-                    <div className='lec_he2' >{
-                        {
-                            1: <p>Basic Python</p>,
-                            2: <p>Web Page Structure</p>,
-                            3: <p>Web Crawling</p>
-                        }[sequence]
-                    }</div>
+            <div className='lec_nav'>
+                <div className='lec_bo1'><p>{data.lecture_content_title}</p></div>
+
+                <div className='lec_bo2'><p>{data.lecture_content}</p></div>
+                <div className='lec_bo33'>
+                    <div><p className='lec_bo3'>{data.like_count}</p></div>
+                    <div><p className='lec_bo4'>{data.create_time}</p></div>
                 </div>
 
-                <div class='lec_nav'>
-                    <div class='lec_bo1'>{data.lecture_content_title}</div>
-                    <br></br>
-                    <div class='lec_bo2'>{data.lecture_content}</div>
-                    <br></br>
-                    <div class='lec_bo3'>{data.like_count}</div>
-                    <div class='lec_bo4'> {data.create_time}</div>
 
+            </div>
+
+            <div className='lec_body'>
+
+                <div className='lec_article'>
+
+
+                    <button className='clickedStudy' onClick={() => {
+                        attending_lecture()
+                        getUserInfo2()
+                    }}>수강 하기</button>
+
+                    <button className="button_QA" disabled={disable} ><Link to={{
+                        pathname: "/codeEdit",
+                        state: {
+                            lecture_content_seq: data.lecture_content_seq,
+                        }
+                    }}>
+                        실습 하기
+                    </Link></button>
+                    <div className="userState">{attending === 0 ? "수강 전" : "수강 중"}</div>
                 </div>
 
-                <div class='lec_body'>
-
-                    <div class='lec_article'>
-
-
-                        <button class='clickedStudy' onClick={attending_lecture} >수강 하기</button>
-
-                        <Link to={{
-                            pathname: "/codeEdit",
-                            state: {
-                                lecture_content_seq: data.lecture_content_seq,
-                            }
-                        }}>
-                            <button class="button_QA" disabled={disable}>실습 하기</button>
-                        </Link>
-
-                    </div>
-
-                    <div class='lec_section'>
+                <div className='lec_section'>
+                    <div className='lec_section2'>
                         <ReactMarkdown rehypePlugins={[rehypeRaw]} children={lecture_content.toString()}></ReactMarkdown>
                     </div>
 
                 </div>
 
+            </div>
 
-
-
-            </body>
         </div>
     );
 }
