4f64ae64af3079fbf5c810ee0afbb29419ef1e12
RicardoKim <may3210@naver.com>
2022-05-31 22:38:20 +0900

    배포 api-config 수정

--- client/src/config/api-config.js
+++ client/src/config/api-config.js
@@ -2,6 +2,8 @@ let backendhost;
 
 const hostname = window && window.location && window.location.hostname;
 
-backendhost = "http://ec2-3-34-43-169.ap-northeast-2.compute.amazonaws.com:8080";
+if (hostname === "localhost"){
+    backendhost = "http://ec2-3-34-43-169.ap-northeast-2.compute.amazonaws.com:8080";
+}
 
 export const API_BASE_URL = `${backendhost}`
